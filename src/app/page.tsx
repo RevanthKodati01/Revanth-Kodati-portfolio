1cd0286788f45f9062d9a6a454bc123de1eb5527
RevanthKodati01 <kodatirevanth@gmail.com>
2026-02-19 23:21:27 -0500

    Updated UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -177,8 +177,8 @@ export default function Page() {
                   yOffset={8}
                   text={DATA.name} 
                 />
-                <BlurFade delay={BLUR_FADE_DELAY}>
-                  {/* <div className="flex items-center gap-2">
+                {/* <BlurFade delay={BLUR_FADE_DELAY}>
+                  <div className="flex items-center gap-2">
                     <span className="relative flex h-2 w-2">
                       <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-black dark:bg-white opacity-75"></span>
                       <span className="relative inline-flex rounded-full h-2 w-2 bg-black dark:bg-white"></span>
@@ -186,8 +186,8 @@ export default function Page() {
                     <span className="text-xs font-mono text-black/70 dark:text-white/70 tracking-[0.2em] uppercase">
                       System.Inference_Active // Port:8000
                     </span>
-                  </div> */}
-                </BlurFade>
+                  </div>
+                </BlurFade> */}
               </div>
               <BlurFadeText
                 className="text-muted-foreground max-w-[600px] md:text-lg lg:text-xl font-sans"
